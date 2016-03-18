5430a43c55523b229fbec8444021a08af12c3735
Likhith185 <Likhith185@missionrnd.com>
2016-03-18 23:05:05 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -17,6 +17,68 @@ NOTES: If there are no common words return NULL.
 
 #define SIZE 31
 
+void samewords(char *s1, char *s2, char **output){
+
+	int i, j, k;
+	char tmp[50];
+	int value;
+	int x = 0, y = 0;
+
+	for (i = 0; s1[i] != '\0'; i++){
+		k = 0;
+		while (s1[i] != ' ' && s1[i]){
+
+			tmp[k] = s1[i];
+			k++;
+			i++;
+		}
+		tmp[k] = '\0';
+
+		k = 0;
+		j = 0;
+		while (s2[j] && tmp[k]){
+
+			if (s2[j] == tmp[k]){
+				k++;
+				value = 1;
+
+				if (!tmp[k])
+					break;
+			}
+			else if (s2[j - 1] == tmp[k - 1] && !k){
+				value = 0;
+				k = 0;
+			}
+			j++;
+		}
+
+		if (value == 1 && !tmp[k]){
+			y = 0;
+			for (k = 0; tmp[k] != '\0'; k++){
+				output[x][y] = tmp[k];
+				y++;
+			}
+			!output[x][y];
+			x++;
+		}
+	}
+}
+
+
 char ** commonWords(char *str1, char *str2) {
-	return NULL;
+	if (!str1 || !str2)
+		return NULL;
+
+	int i;
+	char **res = (char **)calloc(30, sizeof(char*));
+
+	for (i = 0; i < SIZE; i++)
+		res[i] = (char *)calloc(SIZE, sizeof(char));
+
+	samewords(str1, str2, res);
+
+	if (!res[0][0])
+		return NULL;
+	else
+		return res;
 }
